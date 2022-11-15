675abf6c8b72347e0956ab60cd68ac3031379b35
Francopress26 <francopress26@hotmail.com>
2022-11-15 17:39:54 -0300

    setup routes

---
+++ api/src/routes/index.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+const router = Router();
+
+const routePost = require("./post");
+const routeUser = require("./user");
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+
+
+
+
+ router.use('/post', routePost);
+router.use("/user", routeUser);
+
+
+
+module.exports = router;
---
+++ api/src/routes/user.js
@@ -0,0 +1,6 @@
+const { Router} = require('express');
+const router = Router();
+require('dotenv').config();
+const axios = require("axios");
+
+module.exports=router
